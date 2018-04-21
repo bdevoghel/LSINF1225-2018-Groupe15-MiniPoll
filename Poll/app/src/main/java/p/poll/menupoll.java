2c575a2731cbd12a28820a8e5c7aebfc915fdb60
chauvauxn <37975707+chauvauxn@users.noreply.github.com>
2018-04-21 14:44:33 +0200

    Add menupoll
    Classe qui contient toutes les fonctions de base que constitue un poll

---
+++ Poll/app/src/main/java/p/poll/menupoll.java
@@ -0,0 +1,18 @@
+package be.lsinf1225.PoolApp;
+
+import java.text.DateFormat;
+
+/**
+ * Created by Nicolas on 21/04/2018.
+ */
+
+public class menupoll
+{
+    String Title;
+    String Description;
+    DateFormat DeadLine;
+    Character Type;
+    User Proprietaire;
+    List<User> ListeAcces;
+    int ID;
+}
