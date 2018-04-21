c3911a69ba545efc7b59ceaf0fbae2d9cfc31633
chauvauxn <37975707+chauvauxn@users.noreply.github.com>
2018-04-21 17:56:07 +0200

    add Node class

--- Poll/app/src/main/java/p/poll/Poll.java
+++ Poll/app/src/main/java/p/poll/Poll.java
@@ -14,6 +14,7 @@ import be.lsinf1225.PoolApp.database.Database;
  */
 
 public class Poll {
+    static int number;
     String title;
     String description;
     Date deadLine;
@@ -125,5 +126,93 @@ public class Poll {
     public void chooseBySliding()
     {
 
+    }
+}
+
+class ListNumber
+{
+    Node first;
+    public ListNumber()
+    {
+        first = null;
+    }
+
+    int remove()
+    {
+        if(first == null)
+        {
+            return -1;
+        }
+        else
+        {
+            int value = first.number;
+            first = first.next;
+            return value;
+        }
+    }
+
+    void insert(int number)
+    {
+        Node current = first;
+        if(first == null)
+        {
+            first = new Node(number);
+        }
+        else
+        {
+            if(first.next == null)
+            {
+                if(first.number < number)
+                {
+                    Node ajout = new Node(number);
+                    first.next = ajout;
+                    return;
+                }
+                else
+                {
+                    Node ajout = new Node(number);
+                    ajout.next = first;
+                    first = ajout;
+                    return;
+                }
+            }
+            if(first.number > number)
+            {
+                Node ajout = new Node(number);
+                ajout.next = first;
+                first = ajout;
+            }
+            else
+            {
+                while(current.next != null)
+                {
+                    if(current.next.number < number)
+                    {
+                        current = current.next;
+                    }
+                    else
+                    {
+                        Node ajout = new Node(number);
+                        ajout.next = current.next;
+                        current.next = ajout;
+                        return;
+                    }
+                }
+                Node ajout = new Node(number);
+                current.next = ajout;
+            }
+        }
+    }
+}
+
+
+class Node
+{
+    int number;
+    Node next;
+    public Node(int num)
+    {
+        this.number = num;
+        next = null;
     }
 }
