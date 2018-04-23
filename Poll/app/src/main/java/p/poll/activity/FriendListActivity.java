466e8e534dd082fd5c3606375b45e3409a15fe80
Vahid <vahid.beyraghi@student.uclouvain.be>
2018-04-23 19:24:26 +0200

    Revert "update friend"
    This reverts commit 18b206d95fa749392ebde8039c18b3f7347587f7.

--- Poll/app/src/main/java/p/poll/activity/FriendListActivity.java
+++ Poll/app/src/main/java/p/poll/activity/FriendListActivity.java
@@ -2,9 +2,13 @@ package p.poll.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
+import android.graphics.Picture;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,8 +21,8 @@ public class FriendListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friendlist);
-        final Button button = findViewById(R.id.button1);
+        setContentView(R.layout.activity_friend);
+        final Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             }
