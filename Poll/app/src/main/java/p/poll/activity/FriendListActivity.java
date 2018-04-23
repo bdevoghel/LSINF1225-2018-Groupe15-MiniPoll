26acb6809c0f40a8b5eb3bf51fae75eb5ced11d1
Vahid <vahid.beyraghi@student.uclouvain.be>
2018-04-23 17:11:01 +0200

    Manifest

--- Poll/app/src/main/java/p/poll/activity/FriendListActivity.java
+++ Poll/app/src/main/java/p/poll/activity/FriendListActivity.java
@@ -5,16 +5,20 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.graphics.Picture;
 
 import p.poll.R;
+import p.poll.model.User;
 
 public class FriendListActivity extends AppCompatActivity {
 
+    private Map<String,User> userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
     }
-
 }
