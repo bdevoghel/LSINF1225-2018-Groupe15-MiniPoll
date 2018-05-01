345f1314e3acb2c7fafabed4d49bbefbd9feabac
Nicolas <n.chauvaux@student.uclouvain.be>
2018-05-01 11:36:03 +0200

    change database V2.0

--- Poll/app/src/main/java/p/poll/MySQLiteHelper.java
+++ Poll/app/src/main/java/p/poll/MySQLiteHelper.java
@@ -29,11 +29,11 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
      * Nom du fichier sql contenant les instructions de création de la base de données. Le fichier
      * doit être placé dans le dossier assets/
      */
-    private static final String DATABASE_SQL_FILENAME = "database.sql";
+    private static final String DATABASE_SQL_FILENAME = "base.sql";
     /**
      * Nom du fichier de la base de données.
      */
-    private static final String DATABASE_NAME = "musicplayer_database.sqlite";
+    private static final String DATABASE_NAME = "easypoll_database.sqlite";
 
     /**
      * Version de la base de données (à incrémenter en cas de modification de celle-ci afin que la
