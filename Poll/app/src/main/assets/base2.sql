db04f34f4d662295aecb39f474517d370ac96bb5
vahidbe <vahidbe@users.noreply.github.com>
2018-05-05 20:24:37 +0200

    BASE NOT NULL SUPPRIME
    il faudra rajouter les not null plus tard

--- Poll/app/src/main/assets/base2.sql
+++ Poll/app/src/main/assets/base2.sql
@@ -2,7 +2,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS Utilisateur;
-CREATE TABLE Utilisateur(Username TEXT NOT NULL UNIQUE PRIMARY KEY, Prenom TEXT NOT NULL, Nom TEXT NOT NULL, email TEXT NOT NULL, mdp TEXT NOT NULL, photo NUMERIC DEFAULT 'default.jpeg', favori INTEGER);
+CREATE TABLE Utilisateur(Username TEXT NOT NULL UNIQUE PRIMARY KEY, Prenom TEXT, Nom TEXT, email TEXT, mdp TEXT NOT NULL, photo NUMERIC DEFAULT 'default.jpeg', favori INTEGER);
 INSERT INTO Utilisateur(Username,Prenom,Nom,email,mdp) VALUES('rogerdu','roger','dupont','r.dupont@yopmail.com','ilovesqlite');
 
 COMMIT TRANSACTION;
--- Poll/app/src/main/assets/baseguigui.sql
+++ Poll/app/src/main/assets/baseguigui.sql
@@ -3,8 +3,8 @@ BEGIN TRANSACTION;
 
 -- Table: Utilisateur
 DROP TABLE IF EXISTS Utilisateur;
-CREATE TABLE Utilisateur (Mail TEXT NOT NULL UNIQUE PRIMARY KEY, Nom TEXT NOT NULL, Prénom TEXT NOT NULL, "Mot de passe" TEXT NOT NULL, Photo NUMERIC DEFAULT 'default.jpeg' NOT NULL, "Meilleur ami" TEXT, FOREIGN KEY ("Meilleur ami") REFERENCES Utilisateur (Mail));
-INSERT INTO Utilisateur (Mail, Nom, Prénom, "Mot de passe", Photo, "Meilleur ami") VALUES ('harry.smith@mymail.com', 'Smith', 'Harry', 'hsmIth123', 'default.jpeg', 'arthur98@gmail.com');
+CREATE TABLE Utilisateur (Username TEXT NOT NULL UNIQUE PRIMARY KEY, Nom TEXT NOT NULL, Prénom TEXT NOT NULL, mdp TEXT NOT NULL, Photo NUMERIC DEFAULT 'default.jpeg' NOT NULL, "Meilleur ami" TEXT, FOREIGN KEY ("Meilleur ami") REFERENCES Utilisateur (Username));
+INSERT INTO Utilisateur (Username, Nom, Prénom, mdp, Photo, "Meilleur ami") VALUES ('michel', 'Smith', 'Harry', '12345', 'default.jpeg', 'arthur98@gmail.com');
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
