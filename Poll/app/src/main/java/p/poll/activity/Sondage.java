e79f92e2b85aa291d26c81600a53766076c07721
Nicolas <n.chauvaux@student.uclouvain.be>
2018-05-05 18:47:22 +0200

    sondage v4.0

--- Poll/app/src/main/java/p/poll/activity/Sondage.java
+++ Poll/app/src/main/java/p/poll/activity/Sondage.java
@@ -1,11 +1,16 @@
 package p.poll.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import p.poll.R;
 
@@ -23,13 +28,15 @@ public class Sondage extends Activity {
             "Proposition5",
     } ;
 
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_sondage);
         final CustomSondage listAdapter = new
                 CustomSondage(Sondage.this, web);
-        list=(ListView)findViewById(R.id.list);
+        list=(ListView)findViewById(R.id.listView1);
         list.setAdapter(listAdapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -38,5 +45,54 @@ public class Sondage extends Activity {
                 Toast.makeText(Sondage.this, "You Clicked at " +web[+ position]+" value = ", Toast.LENGTH_SHORT).show();
             }
         });
+
+        mListView = (ListView) findViewById(R.id.listView2);
+
+        List<PollModel> polls = genererAmi();
+
+        PollAdapter adapter = new PollAdapter(Sondage.this, polls);
+        mListView.setAdapter(adapter);
+        mListView.setClickable(true);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Object o = mListView.getItemAtPosition(position);
+                PollModel j = (PollModel)o;
+                Toast.makeText(Sondage.this, j.getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private List<PollModel> genererAmi(){
+        List<PollModel> tweets = new ArrayList<PollModel>();
+        tweets.add(new PollModel(Color.BLACK, "Florent", "Mon premier tweet !"));
+        tweets.add(new PollModel(Color.BLUE, "Kevin", "C'est ici que ça se passe !"));
+        tweets.add(new PollModel(Color.GREEN, "Logan", "Que c'est beau..."));
+        tweets.add(new PollModel(Color.RED, "Mathieu", "Il est quelle heure ??"));
+        tweets.add(new PollModel(Color.GRAY, "Willy", "On y est presque"));
+        tweets.add(new PollModel(Color.BLACK, "Florent", "Mon premier tweet !"));
+        tweets.add(new PollModel(Color.BLUE, "Kevin", "C'est ici que ça se passe !"));
+        tweets.add(new PollModel(Color.GREEN, "Logan", "Que c'est beau..."));
+        tweets.add(new PollModel(Color.RED, "Mathieu", "Il est quelle heure ??"));
+        tweets.add(new PollModel(Color.GRAY, "Willy", "On y est presque"));
+        tweets.add(new PollModel(Color.BLACK, "Florent", "Mon premier tweet !"));
+        tweets.add(new PollModel(Color.BLUE, "Kevin", "C'est ici que ça se passe !"));
+        tweets.add(new PollModel(Color.GREEN, "Logan", "Que c'est beau..."));
+        tweets.add(new PollModel(Color.RED, "Mathieu", "Il est quelle heure ??"));
+        tweets.add(new PollModel(Color.GRAY, "Willy", "On y est presque"));
+        tweets.add(new PollModel(Color.BLACK, "Florent", "Mon premier tweet !"));
+        tweets.add(new PollModel(Color.BLUE, "Kevin", "C'est ici que ça se passe !"));
+        tweets.add(new PollModel(Color.GREEN, "Logan", "Que c'est beau..."));
+        tweets.add(new PollModel(Color.RED, "Mathieu", "Il est quelle heure ??"));
+        tweets.add(new PollModel(Color.GRAY, "Willy", "On y est presque"));
+        tweets.add(new PollModel(Color.BLACK, "Florent", "Mon premier tweet !"));
+        tweets.add(new PollModel(Color.BLUE, "Kevin", "C'est ici que ça se passe !"));
+        tweets.add(new PollModel(Color.GREEN, "Logan", "Que c'est beau..."));
+        tweets.add(new PollModel(Color.RED, "Mathieu", "Il est quelle heure ??"));
+        tweets.add(new PollModel(Color.GRAY, "Willy", "On y est presque"));
+        return tweets;
     }
 }
